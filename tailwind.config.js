c1fe7fe3e10e4f4cab5a0f8be98205ad5285cb77
Jie Burillo <benjieburillo10@gmail.com>
2023-06-30 09:53:50 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+import { fontFamily as _fontFamily } from 'tailwindcss/defaultTheme'
+export const content = [
+  "./index.html",
+  "./src/**/*.{vue,js,ts,jsx,tsx}",
+]
+export const theme = {
+  extend: {
+    fontFamily: {
+      sans: ['Inter var', ..._fontFamily.sans],
+    },
+  },
+}
+export const plugins = []
+
